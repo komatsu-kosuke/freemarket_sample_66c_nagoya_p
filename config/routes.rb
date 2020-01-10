2a615fc4156d1ee9f967c6a77e16304d7e82f041
komatsu-kosuke <rainbowbell0127@gmail.com>
2020-01-10 15:59:27 +0900

    db

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :reviews
-  devise_for :tradings
-  devise_for :orders
-  devise_for :comments
-  devise_for :likes
-  devise_for :categories
-  devise_for :sizes
-  devise_for :brands
-  devise_for :product_images
-  devise_for :creditcards
-  devise_for :shippings
-  devise_for :products
-  devise_for :addresses
-  devise_for :profiles
   devise_for :users
   root to: 'homes#index'
 
