25bebe784f53483e078b0951f19ec59494537d84
kazprog109 <kazjeep1996@gmail.com>
2020-01-10 18:36:14 +0900

    db_migrate

---
+++ db/migrate/20200110080432_create_profiles.rb
@@ -0,0 +1,16 @@
+class CreateProfiles < ActiveRecord::Migration[5.2]
+  def change
+    create_table :profiles do |t|
+      t.string :firstname, null: false
+      t.string :familyname, null: false
+      t.string :firstname_kana, null: false
+      t.string :familyname_kana, null: false
+      t.integer :birthyear, null: false
+      t.integer :birthmonth, null: false
+      t.integer :birthday, null: false
+      t.integer :phonenumber, null: false
+      t.references :user, foreign_key: true
+      t.timestamps
+    end
+  end
+end
