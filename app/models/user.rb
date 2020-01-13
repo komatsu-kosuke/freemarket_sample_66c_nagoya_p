ef34ef45619d95622b00dfc9bc96f173f273c53f
kazprog109 <kazjeep1996@gmail.com>
2020-01-13 11:36:05 +0900

    db

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true
+
+
+  has_many :messages
+  has_many :add
+  has_many :groups, through: :group_users
+
 end
