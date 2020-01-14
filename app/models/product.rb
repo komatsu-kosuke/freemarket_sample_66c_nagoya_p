e76365e2cdbdf7cfa3dfe080b08b3f4c29184dae
kazprog109 <kazjeep1996@gmail.com>
2020-01-14 11:27:25 +0900

    model-validate

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
+  has_one :shipping
+  has_one :order
+  has_many :likes
+  has_many :comments
+  has_many :images
   belongs_to :user
   belongs_to :brand
   belongs_to :size
