340344a0cbcd05e077a61381918d1c6ed2c34886
kazprog109 <kazjeep1996@gmail.com>
2020-01-14 17:15:22 +0900

    products_list

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,8 +1,14 @@
 class HomesController < ApplicationController
 
   def index
+    # @products_image.image="#{@product.id}.jpg"
+    # image=params[:image_file]
+    # File.binwrite("public/#{@user.image_name}", image.read)
   end
 
   def show
+  end
+
+  def new
   end
 end
